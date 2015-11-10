8df676ab92d84764e7f9410b958811b4e0d027d8
mdkling <mdkling@gmail.com>
2015-11-10 18:45:27 +0000

    Removed test comments, added real comments.

--- family_clock.rb
+++ family_clock.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 # Simple "Weasly Clock" implementation using
 # ifftt.com 'iOS Location'>>'Maker Channel' REST requests to a Sinatra backend
-# managed by github? Works!
-# c9-test branch? Works!
+# A secondary thread handles the external control logic in parallel
+# including an (optional) hard-coded schedule
 
 require 'sinatra'
 require 'date'
