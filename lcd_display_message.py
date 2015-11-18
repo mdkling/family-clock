b6c04ce8d5ef6836b458eb0da2529cfb4e3fd28d
mdkling <mdkling@gmail.com>
2015-11-18 18:41:07 +0000

    New file added to control the LCD screen via escaped python.

---
+++ lcd_display_message.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+# Python display control for the 16x2 LCD display
+# m. kling | 2015-11-18
+
+# Takes 2 command-line parameters: message and color
+
+import sys
+import Adafruit_CharLCD as LCD
+
+lcd = LCD.Adafruit_CharLCDPlate()
+
+lcd.clear()
+
+color = str(sys.argv[1])
+
+if color == "red":
+    lcd.set_color(1,0,0)
+elif color == "green":
+    lcd.set_color(0,1,0)
+elif color == "blue":
+    lcd.set_color(0,0,1)
+elif color == "yellow":
+    lcd.set_color(1,1,0)
+elif color == "magneta":
+    lcd.set_color(1,0,1)    
+elif color == "cyan":
+    lcd.set_color(0,1,1)
+elif color == "white":
+    lcd.set_color(1,1,1)
+elif color == "off":
+    lcd.set_color(0,0,0)
+else:
+    lcd.set_color(0,0,0)
+    
+lcd.message(sys.argv[1])
